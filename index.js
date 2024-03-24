2b101486c2d74d26595c983ed091847e6cedb120
l3xst1ng <howdy.martt@gmail.com>
2024-03-24 16:22:45 +0300

    Arithimetic lab solution

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
 // Write your code here
 
-// describe('index.js', function() {
-//   describe('multiply', function() {
-//     it("is an equation whose multiplied results will equal 62", function() {
-//       expect(num1).to.be.a('number')
-//       expect(num2).to.be.a('number')
-//       expect(multiply).to.eq(62)
-
-// function multiply(num1, num2) {
-//   num1 = 31;
-//   num2 = 2;
-//   return num1 * num2;
-// }
-// let result = multiply();
-// console.log(result);
-
-// function multiply(num1, num2) {
-//   let result = num1 * num2;
-//   console.log(result);
-//   return result;
-// }
-
-// multiply(2, 31);
-
 let num1 = 31;
 let num2 = 2;
 let multiply = num1 * num2;
